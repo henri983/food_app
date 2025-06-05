388bf1296ec854f03f98c7513439b94c1f73cf92
reinebk <reineasseng@gmail.com>
2025-06-05 14:19:14 +0200

    site 18h33

---
+++ profil.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="./style/style.css">
+    <title>Profile</title>
+</head>
+<body>
+      <?php require_once "header.php"; ?>
+  
+    <main>
+        <section class="content">
+            <h2>Profil</h2>
+            <div class="container">
+                <?php
+                session_start();
+                if (isset($_SESSION['email'])) {
+                    echo "<p>Bienvenue, " . htmlspecialchars($_SESSION['email']) . "!</p>";
+                    echo '<a href="logout.php" class="crystal-btn">Se déconnecter</a>';
+                } else {
+                    echo "<p>Vous n'êtes pas connecté.</p>";
+                    echo '<a href="login.php" class="crystal-btn">Se connecter</a>';
+                }
+                ?>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <?php require_once "footer.php"; ?>
+    </footer>
+</body>
+</html>
