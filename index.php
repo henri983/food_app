841eaeb30fda1c3e4c76ca76d4f5b5f419ecb9b3
reinebk <reineasseng@gmail.com>
2025-06-07 12:49:24 +0200

    style_12h49

--- index.php
+++ index.php
@@ -90,6 +90,7 @@ if (isset($_SESSION['error'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notre Menu Complet</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <?php require_once 'nav_bar.php'?>
